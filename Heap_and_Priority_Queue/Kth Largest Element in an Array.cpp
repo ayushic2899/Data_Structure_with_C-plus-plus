aa4395c9e2951f40d39fb3b11a446a0ecfaee9db
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-03 12:23:05 +0530

    Create Kth Largest Element in an Array.cpp

---
+++ Heap_and_Priority_Queue/Kth Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+  
+  `````
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>minh;
+        for(int i=0;i<nums.size();i++){
+            minh.push(nums[i]);
+            if(minh.size() > k){
+                minh.pop();
+            }
+        }
+        return minh.top();
+    }
+};
+
+ 
