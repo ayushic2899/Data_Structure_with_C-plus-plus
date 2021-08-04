06924953adbb717626b4e9b345cdf546ab13881f
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-04 14:09:58 +0530

    Create Minimum Path Sum.cpp

---
+++ Dynamic_Programming/Minimum Path Sum.cpp
@@ -0,0 +1,32 @@
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+         int m=grid.size();
+        int n=grid[0].size();
+         int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                }
+                else if(i==0){
+                    dp[0][j]=dp[0][j-1] + grid[0][j];
+                }
+                else if(j==0){
+                    dp[i][0]=dp[i-1][0]+grid[i][0];
+                }
+                else{
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+                }
+            }
+        }
+        return dp[m-1][n-1]; 
+    }
+};
+
+ 
