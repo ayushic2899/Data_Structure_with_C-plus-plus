9fca833afacafb73babaef566c65de41f7d9ed27
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-20 19:28:45 +0530

    Create Longest Substring with At Least K Repeating Characters.cpp

---
+++ String/Longest Substring with At Least K Repeating Characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        int n= s.length();
+        if(n ==0 || n<k){
+            return 0;
+        }
+        if(k <=1){
+            return n;
+        }
+        unordered_map<char,int>count;
+        for(char c: s){
+            count[c]+=1;
+        }
+        int l=0;
+        while(l<n && count[s[l]]>=k){
+            l++;
+        }
+            if(l>=n-1){
+                return l;
+            }
+            int ls1=longestSubstring(s.substr(0,l),k);
+            while(l<n && count[s[l]]<k){
+                l++;
+            }
+            int ls2=(l<n)?longestSubstring(s.substr(l),k):0;
+     return max(ls1,ls2);
+    }
+};
+
+ 
