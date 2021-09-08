eac18140cdd513278c795a38a4f254002226e6d7
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-09-09 01:21:13 +0530

    Create 1323. Maximum 69 Number.cpp

---
+++ Array/1323. Maximum 69 Number.cpp
@@ -0,0 +1,18 @@
+link-https://leetcode.com/problems/maximum-69-number/
+
+`````````````````
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s=to_string(num);
+        for(auto &i:s){
+            if(i=='6'){
+                i='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
+ 
